cdf2d75c7a3c305820a5c8df0abb6788c8779f35
butterbean <butterbean@mail.ru>
2021-05-07 12:55:43 +0300

    start

---
+++ app/src/main/java/ru/butterbean/tut_telegram/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.butterbean.tut_telegram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.butterbean.tut_telegram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
