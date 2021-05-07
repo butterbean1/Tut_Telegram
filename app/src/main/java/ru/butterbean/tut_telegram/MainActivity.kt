751c28feb7906f1c45102c732e9be63da23c1a9d
butterbean <butterbean@mail.ru>
2021-05-07 13:26:26 +0300

    start

--- app/src/main/java/ru/butterbean/tut_telegram/MainActivity.kt
+++ app/src/main/java/ru/butterbean/tut_telegram/MainActivity.kt
@@ -7,7 +7,7 @@ import ru.butterbean.tut_telegram.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding:ActivityMainBinding
-    val f=1
+    val f=12
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
