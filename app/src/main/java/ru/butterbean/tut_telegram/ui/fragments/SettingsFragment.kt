b33ca387fb48ee1d2566f901c79e6978fa54e72e
butterbean <butterbean@mail.ru>
2021-05-08 16:41:55 +0300

    full 7 part

--- app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/SettingsFragment.kt
@@ -1,11 +1,16 @@
 package ru.butterbean.tut_telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import ru.butterbean.tut_telegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     override fun onResume() {
         super.onResume()
-
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
+    }
 }
