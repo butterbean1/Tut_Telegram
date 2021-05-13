34741be1cd1e0990d1822adf2f6da42ffb9e13a8
butterbean1 <butterbean@mail.ru>
2021-05-13 23:01:21 +0300

    full 35 part. toolbar almost complete

---
+++ app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/SingleChatFragment.kt
@@ -0,0 +1,16 @@
+package ru.butterbean.tut_telegram.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.butterbean.tut_telegram.R
+import ru.butterbean.tut_telegram.utilites.APP_ACTIVITY
+
+class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
