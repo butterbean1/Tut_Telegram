fb0029d9d41decc9fe351e8f6363dd80837689fd
butterbean <butterbean@mail.ru>
2021-05-15 14:45:04 +0300

    full real !! 39 part

--- app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/ChatsFragment.kt
+++ app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/ChatsFragment.kt
@@ -2,10 +2,12 @@ package ru.butterbean.tut_telegram.ui.fragments
 
 import androidx.fragment.app.Fragment
 import ru.butterbean.tut_telegram.R
+import ru.butterbean.tut_telegram.utilites.APP_ACTIVITY
 
 class ChatsFragment : Fragment(R.layout.fragment_chats) {
     override fun onResume() {
         super.onResume()
+        APP_ACTIVITY.title = "Чаты"
     }
 
 
--- app/src/main/java/ru/butterbean/tut_telegram/utilites/appDatabaseHelper.kt
+++ app/src/main/java/ru/butterbean/tut_telegram/utilites/appDatabaseHelper.kt
@@ -6,6 +6,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ServerValue
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import ru.butterbean.tut_telegram.models.CommonModel
@@ -17,7 +18,10 @@ lateinit var REF_DATABASE_ROOT: DatabaseReference
 lateinit var REF_STORAGE_ROOT: StorageReference
 lateinit var USER: UserModel
 
+const val TYPE_TEXT = "text"
+
 const val NODE_USERS = "users"
+const val NODE_MESSAGES = "messages"
 const val NODE_USERNAMES = "usernames"
 const val NODE_PHONES = "phones"
 const val NODE_PHONES_CONTACTS = "phones_contacts"
@@ -31,6 +35,11 @@ const val CHILD_FULLNAME = "fullname"
 const val CHILD_BIO = "bio"
 const val CHILD_PHOTO_URL = "photoUrl"
 const val CHILD_STATE = "state"
+const val CHILD_TEXT = "text"
+const val CHILD_TYPE = "type"
+const val CHILD_FROM = "from"
+const val CHILD_TIMESTAMP = "timeStamp"
+
 
 fun initFirebase() {
     AUTH = FirebaseAuth.getInstance()
@@ -98,3 +107,24 @@ fun DataSnapshot.getCommonModel(): CommonModel =
 
 fun DataSnapshot.getUserModel(): UserModel =
     this.getValue(UserModel::class.java) ?: UserModel()
+
+fun sendMessage(message: String, receivingUserId: String, typeText: String, function: () -> Unit) {
+    val refDialogUser = "$NODE_MESSAGES/$CURRENT_UID/$receivingUserId"
+    val refDialogReceivingUser = "$NODE_MESSAGES/$receivingUserId/$CURRENT_UID"
+    val messageKey = REF_DATABASE_ROOT.child(refDialogUser).push().key
+    val mapMessage = hashMapOf<String,Any>()
+    mapMessage[CHILD_FROM] = CURRENT_UID
+    mapMessage[CHILD_TYPE] = typeText
+    mapMessage[CHILD_TEXT] = message
+    mapMessage[CHILD_TIMESTAMP] = ServerValue.TIMESTAMP
+
+    val mapDialog = hashMapOf<String,Any>()
+    mapDialog["$refDialogUser/$messageKey"] = mapMessage
+    mapDialog["$refDialogReceivingUser/$messageKey"] = mapMessage
+
+    REF_DATABASE_ROOT
+        .updateChildren(mapDialog)
+        .addOnSuccessListener { function() }
+        .addOnFailureListener { showToast(it.message.toString()) }
+
+}
