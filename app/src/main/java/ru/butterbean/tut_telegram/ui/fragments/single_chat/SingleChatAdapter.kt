2dc97e5118ceca74f55893bf5bb30d883f5a67ae
butterbean1 <butterbean@mail.ru>
2021-05-18 11:33:38 +0300

    full 43 part

--- app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/single_chat/SingleChatAdapter.kt
+++ app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/single_chat/SingleChatAdapter.kt
@@ -64,7 +64,8 @@ class SingleChatAdapter: RecyclerView.Adapter<SingleChatAdapter.SingleChatHolder
     fun addItem(item:CommonModel){
         val newList = mutableListOf<CommonModel>()
         newList.addAll(mListMessagesCache)
-        newList.add(item)
+        if (!newList.contains(item)) newList.add(item)
+        newList.sortBy { it.timeStamp.toString() }
         mDiffResult = DiffUtil.calculateDiff(DiffUtilCallback(mListMessagesCache,newList))
         mDiffResult.dispatchUpdatesTo(this)
         mListMessagesCache = newList
--- app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/single_chat/SingleChatFragment.kt
+++ app/src/main/java/ru/butterbean/tut_telegram/ui/fragments/single_chat/SingleChatFragment.kt
@@ -1,10 +1,9 @@
 package ru.butterbean.tut_telegram.ui.fragments.single_chat
 
 import android.view.View
+import android.widget.AbsListView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.fragment_single_chat.*
@@ -25,8 +24,11 @@ class SingleChatFragment(private val contact: CommonModel) :
     private lateinit var mRefMessages: DatabaseReference
     private lateinit var mAdapter: SingleChatAdapter
     private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mMessagesListener: ChildEventListener
-    private var mListMessages = mutableListOf<CommonModel>()
+    private lateinit var mMessagesListener: AppChildEventListener
+    private var mCountMessages = 10
+    private var mIsScrolling = false
+    private var mSmoothScrollToPosition = true
+    private var mListListeners = mutableListOf<AppChildEventListener>()
 
     override fun onResume() {
         super.onResume()
@@ -43,10 +45,38 @@ class SingleChatFragment(private val contact: CommonModel) :
         mRecyclerView.adapter = mAdapter
         mMessagesListener = AppChildEventListener { snapshot ->
             mAdapter.addItem(snapshot.getCommonModel())
-            mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
+            if (mSmoothScrollToPosition) {
+                mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
+            }
 
         }
-        mRefMessages.addChildEventListener(mMessagesListener)
+        mRefMessages.limitToLast(mCountMessages).addChildEventListener(mMessagesListener)
+        mListListeners.add(mMessagesListener)
+
+        mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (mIsScrolling && dy < 0) {
+                    updateData()
+                }
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    mIsScrolling = true
+                }
+            }
+
+        })
+    }
+
+    private fun updateData() {
+        mSmoothScrollToPosition = false
+        mIsScrolling = false
+        mCountMessages += 10
+        mRefMessages.limitToLast(mCountMessages).addChildEventListener(mMessagesListener)
     }
 
     private fun initToolbar() {
@@ -59,6 +89,7 @@ class SingleChatFragment(private val contact: CommonModel) :
         mRefUser = REF_DATABASE_ROOT.child(NODE_USERS).child(contact.id)
         mRefUser.addValueEventListener(mListenerInfoToolbar)
         chat_btn_send_message.setOnClickListener {
+            mSmoothScrollToPosition = true
             val message = chat_input_message.text.toString()
             if (message.isEmpty()) {
                 showToast("Введите сообщение")
@@ -85,6 +116,9 @@ class SingleChatFragment(private val contact: CommonModel) :
         super.onPause()
         mToolbarInfo.visibility = View.GONE
         mRefUser.removeEventListener(mListenerInfoToolbar)
-        mRefMessages.removeEventListener(mMessagesListener)
+
+        mListListeners.forEach{
+            mRefMessages.removeEventListener(it)
+        }
     }
 }
